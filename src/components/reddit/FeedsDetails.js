e08066b855fd76860850ee381d3beeac2b5f04a1
Penj <penguinjoints@icloud.com>
2023-05-07 14:52:02 +1000

    modify thread display

--- src/components/reddit/FeedsDetails.js
+++ src/components/reddit/FeedsDetails.js
@@ -9,8 +9,16 @@ import {ListGroup, Modal, Pagination} from 'react-bootstrap';
 
 export default function FeedsDetails(props) {
     const [data, setData] = useState([]);
-    const [modalShow, setModalShow] = useState(false);
-    const [imgPerma, setImgPerma] = useState({});
+    const [img, setImg] = useState({
+        show: false,
+        item: {},
+    });
+
+    const [thread, setThread] = useState({
+        show: false,
+        item: {},
+    });
+
 
     const Loading = () => {
         return (
@@ -41,7 +49,7 @@ export default function FeedsDetails(props) {
         return <SuchEmpty/>
     }
     const decodeHtml = (html) => {
-        var txt = document.createElement("textarea");
+        let txt = document.createElement("textarea");
         txt.innerHTML = html;
         return txt.value;
     }
@@ -65,15 +73,50 @@ export default function FeedsDetails(props) {
 
     const handleLinkClick = m => {
         if (isImg(m.url)) {
-            setImgPerma(m);
+            setImg({
+                show: true,
+                item: m
+            });
         } else {
             return window.location = m.url;
         }
-        setModalShow(true);
     }
 
+    const handleThreadLink = () => {
+        setThread({
+            show: false,
+            item: {},
+        });
+        if (isImg(thread.item.url)) {
+            setImg({
+                show: true,
+                item: thread.item
+            });
+        } else {
+            return window.location = `https://www.reddit.com/${thread.item.permalink}`
+        }
+    }
+
+
     const handleTitleClick = m => {
-        return window.location = `https://www.reddit.com/${m.data.permalink}`;
+        console.log(m.data);
+        setThread({
+            show: true,
+            item: m.data,
+        });
+    }
+
+    const handleImgClose = () => {
+        setImg({
+            show: false,
+            item: {}
+        });
+    }
+    const handleThreadClose = () => {
+        setThread({
+            show: false,
+            item: {}
+        });
     }
 
     const handleTimeDisplay = m => {
@@ -131,25 +174,16 @@ export default function FeedsDetails(props) {
                                                 >
                                                     {m.data.title}
                                                 </p>
-                                                <p
-                                                    className="mb-1"
-                                                >
-                                                    {m.kind === "t3" ?
-                                                        parse(decodeHtml(m.data.selftext_html)) : <br/>
-                                                    }
-                                                </p>
                                                 <small className="h6 feeds-author">Posted
                                                     by {m.data.author} at {handleTimeDisplay(m.data)}  </small>
                                             </div>
-                                        </div>
-                                        <div className="container-fulid">
-                                            <span className="mx-1 w-20 bold h4">{handleUpVoteDisplay(m.data.score)}</span>
                                         </div>
                                     </div>
                                 </ListGroup.Item>
                             )
                         )
                 }
+
                 <Pagination className="w-100">
                     <Pagination.Prev
                         className="w-50"
@@ -160,13 +194,48 @@ export default function FeedsDetails(props) {
                         onClick={handleNextClick}/>
                 </Pagination>
             </ListGroup>
-            <Modal size="lg" show={modalShow} onHide={() => setModalShow(false)}>
+
+            <Modal size="lg" show={thread.show} fullscreen={false} onHide={handleThreadClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>{imgPerma.title}</Modal.Title>
+                    <Modal.Title>{thread.item.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <img className="img-fluid" alt="img" src={imgPerma.url}></img>
+                    <div className={"mb-1"} style={{overflow: "hidden"}}>
+                        {
+                            thread.item.selftext === '' ?
+                                handleThreadLink()
+                                : parse(decodeHtml(thread.item.selftext_html))
+                        }
+                    </div>
                 </Modal.Body>
+                <Modal.Footer>
+                    <div className="d-flex justify-content-between">
+                        <p style={{fontSize: "calc(5px + 2vmin)"}}>
+                            by <span
+                            className="h6"> {thread.item.author}</span> at {handleTimeDisplay(thread.item)} with <span
+                            className="h6">{handleUpVoteDisplay(thread.item.score)}</span> upvodes
+                        </p>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+
+            <Modal size="lg" show={img.show} onHide={handleImgClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{img.item.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img className="img-fluid" alt="img" src={img.item.url}></img>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="d-flex justify-content-between">
+                        <p style={{fontSize: "calc(5px + 2vmin)"}}>
+                            by <span
+                            className="h6"> {img.item.author}</span> at <span
+                            className="h6">{handleTimeDisplay(img.item)}</span> with <span
+                            className="h6">{handleUpVoteDisplay(img.item.score)}</span> upvodes
+                        </p>
+                    </div>
+                </Modal.Footer>
             </Modal>
         </div>
     )
